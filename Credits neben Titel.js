81481718c2ec2effb8b42974836b2f6db7420ce3
floflo3299 <59307199+floflo3299@users.noreply.github.com>
2026-04-03 12:05:05 +0200

    Create Credits neben Titel.js

---
+++ Credits neben Titel.js
@@ -0,0 +1,118 @@
+// ==UserScript==
+// @name         Credits neben Titel
+// @version      1.0.0
+// @description  zeigt die Credits-Menge für den Einsatz im Titel an
+// @author       Silberfighter
+// @include      *www.leitstellenspiel.de/missions/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leitstellenspiel.de
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+const STORAGE_KEY = "AllMissionsJSONForLeitstellenspiel"
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+$(document).ready(async function(){
+
+    let missionID = getMissionID();
+
+    let missionInfo = getMissionRequirements(missionID + "-0");
+
+    if(missionInfo == null){
+        missionInfo = getMissionRequirements(missionID);
+    }
+
+    if(missionInfo == null){
+        await getAndSaveAllMissions();
+        missionInfo = getMissionRequirements(missionID);
+    }
+
+
+
+    if("average_credits" in missionInfo){
+        var title = $('#missionH1', document);
+
+        var a = document.createElement("small");
+        a.innerHTML = "; " + missionInfo.average_credits + " Credits";
+        title[0].parentNode.append(a);
+    }
+});
+
+
+
+
+
+function getMissionID(){
+    let missionNumber;
+
+    if(document.getElementById("mission_help").href){
+        missionNumber = document.getElementById("mission_help").href.replace("https://www.leitstellenspiel.de/einsaetze/","");
+
+        missionNumber = missionNumber.replace("?additive_overlays=","/");
+
+        // holt sich die Einsatzdetails
+        if(missionNumber.indexOf('mission_id')>=0){
+            return missionNumber.substring(0,missionNumber.indexOf('mission_id')-1);
+        }
+    }
+
+    return "";;
+}
+
+function getMissionRequirements(missionID){
+    let missions = localStorage.getItem(STORAGE_KEY);
+
+    missions = JSON.parse(missions);
+
+    return missions.find(m => m.id === String(missionID)) ?? null;
+}
+
+async function getAndSaveAllMissions(){
+
+    let missions = await fetch("https://www.leitstellenspiel.de/einsaetze.json").then(r => r.json());
+
+
+    const REMOVE_KEYS = [
+        'place',
+        'place_array',
+        'generated_by',
+        'icons',
+        'prerequisites',
+        'overlay_index',
+        'base_mission_id',
+        'additive_overlays',
+        'mission_categories'
+    ];
+
+    missions = missions.map(obj =>Object.fromEntries( Object.entries(obj).filter(([key]) => !REMOVE_KEYS.includes(key))));
+
+
+    const REMOVE_KEYS_ADDITIONAL = [
+        'expansion_missions_ids',
+        'filter_id',
+        'allow_without_poi',
+        'followup_missions_ids',
+        'patient_specialization_captions',
+        'patient_specialization_ids',
+        'patient_specializations',
+        'allow_without_poi',
+        'allow_without_poi'
+    ];
+
+    missions = missions.map(m => ({
+        ...m,
+        additional: m.additional
+        ? Object.fromEntries(
+            Object.entries(m.additional)
+            .filter(([k]) => !REMOVE_KEYS_ADDITIONAL.includes(k))
+        )
+        : m.additional
+    }));
+
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(missions));
+}
+
+
+
+
